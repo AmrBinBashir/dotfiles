887973d9b6ea4dcc9237a070091d32f441f87ca5
amrbashir <48618675+amrbashir@users.noreply.github.com>
2021-03-03 16:03:37 +0200

    03/03/21 04:03 PM

--- linux/init.sh
+++ linux/init.sh
@@ -8,9 +8,9 @@ discord --start-minimized &
 . ~/.fehbg &
 sxhkd &
 nm-applet &
-xinput set-prop "SynPS/2 Synaptics TouchPad" 339 1 &
-xinput set-prop "SynPS/2 Synaptics TouchPad" 319 1 &
-sh ~/.config/polybar/launch.sh &
+xinput set-prop "SynPS/2 Synaptics TouchPad" $(xinput list-props "SynPS/2 Synaptics TouchPad" | grep "Natural Scrolling Enabled" | awk -F'[()]' 'NR==1 {print $(NF-1)}' ) 1 &
+xinput set-prop "SynPS/2 Synaptics TouchPad" $(xinput list-props "SynPS/2 Synaptics TouchPad" | grep "Tapping Enabled" | awk -F'[()]' 'NR==1 {print $(NF-1)}' ) 1 &
+sh ~/.config/polybar/scripts/launch.sh &
 spotify-tray -m &
 volctl &
 mailspring -b &
